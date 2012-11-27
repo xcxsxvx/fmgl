1b624c40ea2ce347c05a1048c271f759511febe3
CPA-ONE <xcxsxvx@yahoo.com>
2012-11-27 23:14:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmgl::Application.config.secret_token = '06e05e2f47463416c340e7b1025f606cf09bfbad4d3c20e7f2bfb5400c6abf82fd26c255d047f631cd2d0864bb0b1ef842155c63c25e02450a171ba21d1a0423'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fmgl::Application.config.session_store :cookie_store, key: '_fmgl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fmgl::Application.config.session_store :active_record_store
