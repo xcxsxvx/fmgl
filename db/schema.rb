20d942f21be62e43c1384b2d2926b156c02a3869
xcxsxvx <xcxsxvx@yahoo.com>
2012-12-01 00:52:17 +0000

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121130214138) do
+ActiveRecord::Schema.define(:version => 20121130235403) do
 
   create_table "lists", :force => true do |t|
     t.string   "product_name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(:version => 20121130214138) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "name"
